0b49f321acc85aa2936ab89be325f7cf02d90410
Jorge <glph8911@gmail.com>
2024-03-11 11:39:34 -0500

    implementing seed data load

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { ProductsService } from '../products/products.service';
+import { initialData } from './data/seed-data';
+
+@Injectable()
+export class SeedService {
+  constructor(private readonly productsService: ProductsService) {}
+  async runSeed() {
+    await this.loadSeedData();
+    return 'Seed executed';
+  }
+
+  private async loadSeedData() {
+    await this.productsService.deleteAllProducts();
+
+    const products = initialData.products;
+    const insertPromises = products.map((product) =>
+      this.productsService.create(product),
+    );
+    await Promise.all(insertPromises);
+  }
+}
